6b9708d31fedc65c1d97c4dcc18d961758d2c1b1
임동준 <127932430+djlim00@users.noreply.github.com>
2026-02-19 21:41:04 +0900

    첫 커밋이요!

---
+++ src/main/java/dj/devlog/DevLogApplication.java
@@ -0,0 +1,13 @@
+package dj.devlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevLogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevLogApplication.class, args);
+    }
+
+}
